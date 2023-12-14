68b37b0d1ae97091f9b33cf115565d44abacadfa
Marcin Bojara <marcin_5@mail.ru>
2023-12-14 19:01:06 +0100

    add fetch and transform data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,44 @@
+import { useEffect, useState, type ReactNode } from 'react';
+
+import fetchingImg from './assets/data-fetching.png';
+import BlogPosts, { type BlogPost } from './components/BlogPosts.tsx';
+import { get } from './util/http.ts';
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+  if (fetchedPosts) content = <BlogPosts posts={fetchedPosts} />;
+
+  return (
+    <main>
+      <img src={fetchingImg} alt="An abstract image depicting a data fetching process." />
+      {content}
+    </main>
+  );
 }
 
 export default App;
